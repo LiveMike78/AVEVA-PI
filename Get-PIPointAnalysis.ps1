cd9cdec5b56e83fa81ace87545fd1c974f0e4cda
Mike Horrocks <mihorrocks@gmail.com>
2024-09-11 11:40:29 +0100

    Add files via upload

---
+++ Get-PIPointAnalysis.ps1
@@ -0,0 +1,190 @@
+# execute in PowerShell with the following parameters to generate CSV file
+# ./Get-PIPointAnalysis.ps1  | Out-File -FilePath PIPointAnalysis.csv
+
+# generated categories:
+# (recently = in the last $STALE_DAYS days)
+#   New: PI Point has no good data, but was created recently
+#   Empty: PI Point has no good data ever
+#   Stale: PI Point has no good data recently
+#   TO BE IMPLIMENTED -- Flat: PI Point has good data, but it does not change 
+#   Good: PI Point has good data recently
+
+# Load AFSDK  
+$AFSDK = [Reflection.Assembly]::LoadWithPartialName("OSIsoft.AFSDK")
+
+# to analyse subsets of tags, enter search criteria in $SEARCH_CRITERIA.
+# see https://docs.aveva.com/bundle/af-sdk/page/html/pipoint-query-syntax-overview.htm
+# some examples search criteria
+
+$SEARCH_CRITERIA = "*" # << all PI tags
+# $SEARCH_CRITERIA = "PointSource:L" # << lab (L) PI tags
+# $SEARCH_CRITERIA = "PointSource:<>L" # << point source is not lab (L)
+# $SEARCH_CRITERIA = "PointType:Float32" # << float32 PI tags
+# $SEARCH_CRITERIA = "sin*" # << PI tags that start with "sin"
+# $SEARCH_CRITERIA = "PointSource:<>[*]"
+
+$STALE_DAYS = 90        # number of days a tag does not have good data for before it is considered stale,
+# or if the tag was created within this number of days it is considered new
+
+$INCLUDE_GOOD = $true   # include good tags in the output, is useful for full tag analysis
+
+function Get-IsGood {
+    param (
+        $pivalue
+    )
+
+    # Get-IsGood returns True when the value is Shutdown or Scan Off
+    # pass a PIValue object
+    # will return a boolean; true = good, false = bad
+
+    if (($pivalue.value -eq "Shutdown") -or ($pivalue.value -eq "Scan Off") -or ($null -eq $pivalue.value)) {
+        return $false
+    }
+    else {
+        return $pivalue.IsGood
+    }
+}
+
+function Get-LastGoodValue {
+    param (
+        $pitag,
+        $future = $false
+    )
+
+    # return the latest good pi value
+
+    # pass in a PIPoint object
+    # return a PIValue object, or null of there is no good data
+
+    $snapshot = $pitag.CurrentValue()
+
+    if (Get-IsGood($snapshot) -eq $true) {
+        # snapshot is good, return that and do no more    
+        return $snapshot
+    }
+    else {
+        # otherwise we need to search back in time to find the last good value
+        $tag = $pitag.Name
+
+        # start now, look for one value, work backwards from now
+        $startTime = (Get-Date)
+        $count = 1
+        $boundaryType = [OSIsoft.AF.Data.AFBoundaryType]::Inside
+
+        # filter data that is bad or shutdown/scan off
+        $filterExpression = "NOT ((BadVal('" + $tag + "'))" +
+        " OR ('" + $tag + "' = ""Shutdown"")" +
+        " OR ('" + $tag + "' = ""Scan Off""))"
+        
+        $includeFilteredValues = $false
+        
+        # perform the search
+        $pivalues = $pitag.RecordedValuesByCount($startTime, $count, $future, $boundaryType, $filterExpression, $includeFilteredValues)
+
+        if ($null -ne $piValues) {
+            # found a good value
+            if ($pivalues.count -gt 0) {
+                return $pivalues[0]
+            }
+            else {
+                # found no good value
+                return $null
+            }    
+        }
+    }
+}
+
+# PI Data Archive, connect to default PI Server
+$piServers = New-Object OSIsoft.AF.PI.PIServers
+$piServer = $piServers.DefaultPIServer
+
+# get PI Points
+$piPoints = [OSIsoft.AF.PI.PIPoint]::FindPIPoints($piServer, $SEARCH_CRITERIA, $null, $null)
+
+# header for output, match names for PI builder compatibility
+("Name;ObjectType;pointtype;archiving;future;pointsource;scan;creationdate;changedate;lastgoodvalue;lastgoodtimestamp;agedays;category")
+
+# check each PI Point
+foreach ($piPoint in $piPoints) {
+
+    # get attributes
+    $piPoint.LoadAttributes()
+    $pointType = $piPoint.GetAttribute("pointtype")
+    $archiving = $piPoint.GetAttribute("archiving")
+    $future = $piPoint.GetAttribute("future")
+    $pointSource = $piPoint.GetAttribute("pointsource")
+    $scan = $piPoint.GetAttribute("scan")
+    $creationDate = $piPoint.GetAttribute("creationdate")
+    $changeDate = $piPoint.GetAttribute("changedate")
+
+    if ($archiving -eq 1) {
+        # find the last good value in the PI Point
+        if ($future -eq 0) {
+            $lastGoodValue = Get-LastGoodValue -pitag $piPoint
+        }
+        else {
+            $lastGoodValue = Get-LastGoodValue -pitag $piPoint -future $true
+        }
+    }
+    else {
+        # no archiving, only the snapshot is of interest
+        $snapshot = $piPoint.CurrentValue()
+
+        if (Get-IsGood($snapshot) -eq $true) {
+            # snapshot is good, return that and do no more    
+            $lastGoodValue = $snapshot
+        }
+        else {
+            $lastGoodValue = $null
+        }
+    }
+
+    if ($null -eq $lastGoodValue) {
+        # there is no good value
+        $timestamp = ""
+        $category = "Empty"
+        $ageDays = 0
+    }
+    else {   
+        # timestamp for the last good value
+        $timestamp = $lastGoodValue.Timestamp
+
+        $age = (New-TimeSpan -Start $timestamp -End (Get-Date))
+        $ageDays = $age.TotalDays
+
+        # the last good value is old
+        if ($ageDays -gt $STALE_DAYS) {
+            $category = "Stale"
+        }
+        else {
+            $category = "Good"
+        }
+    }
+
+    # summary for output
+    if (($category -ne "Good") -or ($INCLUDE_GOOD -eq $true)) {
+        
+        if ($category -eq "Empty") {
+            # no data, check if tag is created "recently"
+            $age = (New-TimeSpan -Start $creationDate -End (Get-Date))
+            $ageDays = $age.TotalDays
+
+            if ($ageDays -lt $STALE_DAYS) {
+                $category = "New"
+            }
+        }
+
+        $value = $lastGoodValue.Value
+
+        # output
+        if ($null -eq $value) {
+            $value = ""
+        }
+        else {
+            # watch out for new lines
+            $value = $value.toString() -replace "`n", ""
+        }
+  
+        ($piPoint.Name + ";PIPoint;" + $pointType + ";" + $archiving + ";" + $future + ";" + $pointSource + ";" + $scan + ";" + $creationDate + ";" + $changeDate + ";" + $value + ";" + $timestamp + ";" + $ageDays + ";" + $category)
+    }
+}
